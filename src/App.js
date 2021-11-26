7bc4697b92bdecf17340cb1390f5058fe0ebf299
emhuammedb <MUENBI@users.noreply.github.com>
2021-11-26 12:08:41 +0300

    book search app finished

--- src/App.js
+++ src/App.js
@@ -1,23 +1,161 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import {
+  InputGroup,
+  Input,
+  InputGroupAddon,
+  InputGroupText,
+  Button,
+  FormGroup,
+  Label,
+  Spinner,
+} from "reactstrap";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+import axios from "axios";
+import BookCard from "./BookCard.js";
 
 function App() {
+  // States
+  const [maxResults, setMaxResults] = useState(10);
+  const [startIndex, setStartIndex] = useState(1);
+  const [query, setQuery] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [cards, setCards] = useState([]);
+
+  // Handle Search
+  const handleSubmit = () => {
+    setLoading(true);
+    if (maxResults > 40 || maxResults < 1) {
+      toast.error("Max results must be between 1 and 40");
+    } else {
+      axios
+        .get(
+          `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&startIndex=${startIndex}`
+        )
+        .then((res) => {
+          if (startIndex >= res.data.totalItems || startIndex < 1) {
+            toast.error(
+              `Max results must be between 1 and ${res.data.totalItems}`
+            );
+          } else {
+            if (res.data.items.length > 0) {
+              setCards(res.data.items);
+              setLoading(false);
+            }
+          }
+        })
+        .catch((error) => {
+          setLoading(true);
+          toast.error(`${error.response.data.error.message}`);
+        });
+    }
+  };
+
+  // Main Show Case
+  const mainHeader = () => {
+    return (
+      // ! Input -> Center
+      <div className="main-image d-flex justify-content-center align-items-center flex-column">
+        {/* Overlay */}
+        <div className="filter">
+          <h1
+            className="display-2 text-center text-white mb-3"
+            style={{ zIndex: 2 }}
+          >
+            Google Books Search
+          </h1>
+          <div style={{ width: "60%", zIndex: 2 }}>
+            <InputGroup size="lg" className="mb-3">
+              <Input
+                placeholder="Book Search"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+              <InputGroupText>
+                <Button color="secondary" onClick={handleSubmit}>
+                  <i className="fas fa-book-open"></i>
+                </Button>
+              </InputGroupText>
+            </InputGroup>
+            <div className="d-flex text-white justify-content-center">
+              <FormGroup className="ml-5">
+                <Label for="maxResults">Max Results</Label>
+                <Input
+                  type="number"
+                  min="1"
+                  id="maxResults"
+                  placeholder="Max Results"
+                  value={maxResults}
+                  onChange={(e) => setMaxResults(e.target.value)}
+                />
+              </FormGroup>
+              <FormGroup className="ml-5">
+                <Label for="startIndex">Start Index</Label>
+                <Input
+                  type="number"
+                  id="startIndex"
+                  placeholder="Start Index"
+                  value={startIndex}
+                  onChange={(e) => setStartIndex(e.target.value)}
+                />
+              </FormGroup>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const handleCards = () => {
+    console.log(cards);
+    const items = cards.map((item, i) => {
+      let thumbnail = "";
+      if (item.volumeInfo.imageLinks.thumbnail) {
+        thumbnail = item.volumeInfo.imageLinks.thumbnail;
+      }
+
+      return (
+        <div className="col-lg-4 mb-3" key={item.id}>
+          <BookCard
+            thumbnail={thumbnail}
+            title={item.volumeInfo.title}
+            pageCount={item.volumeInfo.pageCount}
+            language={item.volumeInfo.language}
+            authors={item.volumeInfo.authors}
+            publisher={item.volumeInfo.publisher}
+            description={item.volumeInfo.description}
+            previewLink={item.volumeInfo.previewLink}
+            infoLink={item.volumeInfo.infoLink}
+            // 30:58 minutes ----
+          />
+        </div>
+      );
+    });
+
+    if (loading) {
+      return (
+        // ! Spinner -> Bottom
+        <div className="d-flex justify-content-center mt-3">
+          <Spinner style={{ width: "3rem", height: "3rem" }} />
+        </div>
+      );
+    } else {
+      return (
+        <div className="container my-5">
+          <div className="row">{items}</div>
+        </div>
+      );
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-100 h-100">
+      {handleCards()}
+      {mainHeader()}
+      <ToastContainer />
     </div>
   );
 }
